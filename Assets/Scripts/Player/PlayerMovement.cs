d2dc7e39a5da4c50c2a49f1e4ac43ebf178eb345
Joel Porter <andrewjoelporter@gmail.com>
2025-04-19 11:16:05 -0500

    player animations done

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,10 +8,16 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -26,5 +32,14 @@ public class PlayerMovement : MonoBehaviour
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
         _rb.velocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+        
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, _movement.x);
+            _animator.SetFloat(_lastVertical, _movement.y);
+        }
     }
 }
