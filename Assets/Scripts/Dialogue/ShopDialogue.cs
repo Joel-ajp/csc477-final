01d9e4b4cff4e6c29a846de1a5f0c68b43f8ff1d
Ani B <zbaker985@gmail.com>
2025-05-08 20:35:23 -0500

    beginning dialogue logic

---
+++ Assets/Scripts/Dialogue/ShopDialogue.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class ShopDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public DialogueObject currentDialogue;
+    public float textSpeed;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+        index = 0;
+        //Start the dialogue
+        textComponent.text = string.Empty;
+        StartCoroutine(TypeLine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //fast forward text scroll on click
+            if(textComponent.text == currentDialogue.dialogueLines[index].dialogue)
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = currentDialogue.dialogueLines[index].dialogue;
+            }
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        //reset text box
+        textComponent.text = string.Empty;
+
+        //type 1 letter at a time
+        foreach (char c in currentDialogue.dialogueLines[index].dialogue.ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < currentDialogue.dialogueLines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
