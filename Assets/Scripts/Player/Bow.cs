624e248d7ddf2bdc2198a8c11980937c76c16908
Cory Strickland <corystrickland13@gmail.com>
2025-05-14 23:00:14 -0400

    pushing new arrow weapon

--- Assets/Scripts/Player/Bow.cs
+++ Assets/Scripts/Player/Bow.cs
@@ -1,48 +1,116 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
-[RequireComponent(typeof(Animator))]
 public class Bow : MonoBehaviour
 {
-    [Header("Who to read input from")]
+  
     [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private GameObject projectilePrefab;
 
-    [Header("Local-space offsets for each facing")]
-    [Tooltip("Position when player is looking right (Z-rot = 0)")]
     [SerializeField] private Vector2 rightOffset;
-    [Tooltip("Position when player is looking up (Z-rot =  90)")]
-    [SerializeField] private Vector2   upOffset;
-    [Tooltip("Position when player is looking left (Z-rot = 180)")]
+    [SerializeField] private Vector2 upOffset;
     [SerializeField] private Vector2 leftOffset;
-    [Tooltip("Position when player is looking down (Z-rot = –90)")]
     [SerializeField] private Vector2 downOffset;
+
+
+    [SerializeField] private float projectileSpeed = 10f;
+    [SerializeField] private float lifeTime = 5f;
+
+    [SerializeField] private int magazineSize  = 5;
+    [SerializeField] private float reloadDuration = 2f;
+
+    private int _shotsRemaining;
+    private bool _isReloading = false;
+    private PlayerControls _controls;
+
+    private void Awake()
+    {
+        _controls = new PlayerControls();
+        _shotsRemaining = magazineSize;
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+        _controls.Player.Swing.started += OnSwing;
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Swing.started -= OnSwing;
+        _controls.Disable();
+    }
 
     private void Update()
     {
         Vector2 dir = playerMovement.LastMovement;
         if (dir == Vector2.zero) return;
 
-        // decide which way they’re “aiming”
+        // choose facing & offset
         if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
         {
-            // horizontal
-            if (dir.x > 0f)
-                ApplyFacing(0f, rightOffset);
-            else
+            if (dir.x > 0f) {
+                ApplyFacing(  0f, rightOffset);
+            }
+            else{
                 ApplyFacing(180f, leftOffset);
+            }
         }
         else
         {
-            // vertical
-            if (dir.y > 0f)
-                ApplyFacing(90f, upOffset);
-            else
+            if (dir.y > 0f){
+                ApplyFacing( 90f, upOffset);
+            }
+            else{
                 ApplyFacing(-90f, downOffset);
+            };
         }
     }
 
     private void ApplyFacing(float zAngle, Vector2 offset)
     {
-        transform.localEulerAngles   = new Vector3(0f, 0f, zAngle);
-        transform.localPosition      = new Vector3(offset.x, offset.y, transform.localPosition.z);
+        transform.localEulerAngles = new Vector3(0f, 0f, zAngle);
+        transform.localPosition = new Vector3(offset.x, offset.y, transform.localPosition.z);
+    }
+
+     private void OnSwing(InputAction.CallbackContext ctx)
+    {
+        if (_isReloading)
+        {
+            //we could play an animation or something in the ui for reloading
+            Debug.Log("[Bow] Reloading – please wait.");
+            return;
+        }
+
+        if (_shotsRemaining <= 0)
+        {
+            StartCoroutine(ReloadRoutine());
+            return;
+        }
+
+        _shotsRemaining--;
+        Debug.Log($"[Bow] Fired! {_shotsRemaining}/{magazineSize} arrows left.");
+        Shoot();
+
+        if (_shotsRemaining <= 0)
+            StartCoroutine(ReloadRoutine());
+    }
+
+    private void Shoot()
+    {
+        GameObject proj = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
+        //we can change the hard corded projectile speed to the shop bought one
+        //also for lifetime they stay on the screen longer which can be adjusted by a stat
+        rb.velocity = proj.transform.right * projectileSpeed;
+        Destroy(proj, lifeTime);
+    }
+      private IEnumerator ReloadRoutine()
+    {
+        _isReloading = true;
+        yield return new WaitForSeconds(reloadDuration);
+        _shotsRemaining = magazineSize;
+        _isReloading  = false;
     }
 }
--- Assets/Scripts/Player/DamageEnemy.cs
+++ Assets/Scripts/Player/DamageEnemy.cs
@@ -1,21 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageEnemy : MonoBehaviour
 {
     [SerializeField] private int damageAmount = 1;
-    public PlayerStats stats;
-    private void Start()
+    private PlayerStats stats;
+
+    private void Awake()
     {
-        stats = GetComponentInParent<PlayerStats>();
+        // Find the Player by tag and cache its stats
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            stats = player.GetComponent<PlayerStats>();
+            if (stats == null)
+                Debug.LogError("[DamageEnemy] No PlayerStats on the Player object!", player);
+        }
+        else
+        {
+            Debug.LogError("[DamageEnemy] No GameObject tagged 'Player' found in scene!");
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.GetComponent<EnemyHealth>())
+        EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+        if (enemy != null && stats != null)
         {
-            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
-            enemyHealth.TakeDamage(damageAmount + stats.attack_damage);
+            int totalDamage = damageAmount + stats.attack_damage;
+            enemy.TakeDamage(totalDamage);
+            // where we could some indication of destruction of arrow
+            Destroy(gameObject);
         }
     }
 }
