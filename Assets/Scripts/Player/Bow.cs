90596b909438163d2f94f37bfdf93760dca0de56
joel-ajp <andrewjoelporter@gmail.com>
2025-05-15 20:03:37 -0500

    fireball added

--- Assets/Scripts/Player/Bow.cs
+++ Assets/Scripts/Player/Bow.cs
@@ -4,116 +4,149 @@ using System.Collections;
 
 public class Bow : MonoBehaviour
 {
-  
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private Animator playerAnimator;
     [SerializeField] private GameObject projectilePrefab;
-
     [SerializeField] private Vector2 rightOffset;
     [SerializeField] private Vector2 upOffset;
     [SerializeField] private Vector2 leftOffset;
     [SerializeField] private Vector2 downOffset;
-
-
     [SerializeField] private float projectileSpeed = 10f;
     [SerializeField] private float lifeTime = 5f;
-
-    [SerializeField] private int magazineSize  = 5;
+    [SerializeField] private int magazineSize = 5;
     [SerializeField] private float reloadDuration = 2f;
-
+    [SerializeField] private float attackDuration = 0.2f; // Duration of attack animation
+    
     private int _shotsRemaining;
     private bool _isReloading = false;
+    private bool _isAttacking = false;
     private PlayerControls _controls;
-
+    
     private void Awake()
     {
         _controls = new PlayerControls();
         _shotsRemaining = magazineSize;
     }
-
+    
     private void OnEnable()
     {
         _controls.Enable();
         _controls.Player.Swing.started += OnSwing;
     }
-
+    
     private void OnDisable()
     {
         _controls.Player.Swing.started -= OnSwing;
         _controls.Disable();
     }
-
+    
     private void Update()
     {
         Vector2 dir = playerMovement.LastMovement;
         if (dir == Vector2.zero) return;
-
+        
         // choose facing & offset
         if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
         {
             if (dir.x > 0f) {
-                ApplyFacing(  0f, rightOffset);
+                ApplyFacing(0f, rightOffset);
             }
-            else{
+            else {
                 ApplyFacing(180f, leftOffset);
             }
         }
         else
         {
-            if (dir.y > 0f){
-                ApplyFacing( 90f, upOffset);
+            if (dir.y > 0f) {
+                ApplyFacing(90f, upOffset);
             }
-            else{
+            else {
                 ApplyFacing(-90f, downOffset);
-            };
+            }
         }
     }
-
+    
     private void ApplyFacing(float zAngle, Vector2 offset)
     {
         transform.localEulerAngles = new Vector3(0f, 0f, zAngle);
         transform.localPosition = new Vector3(offset.x, offset.y, transform.localPosition.z);
     }
-
-     private void OnSwing(InputAction.CallbackContext ctx)
+    
+    private void OnSwing(InputAction.CallbackContext ctx)
     {
-        if (_isReloading)
+        // Don't allow shooting while attacking or reloading
+        if (_isAttacking || _isReloading)
         {
-            //we could play an animation or something in the ui for reloading
-            Debug.Log("[Bow] Reloading – please wait.");
+            if (_isReloading)
+            {
+                Debug.Log("[Bow] Reloading – please wait.");
+            }
             return;
         }
-
+        
         if (_shotsRemaining <= 0)
         {
             StartCoroutine(ReloadRoutine());
             return;
         }
-
+        
+        // Start the attack
+        StartCoroutine(AttackRoutine());
+    }
+    
+    private IEnumerator AttackRoutine()
+    {
+        _isAttacking = true;
+        
+        // Disable player movement
+        playerMovement.DisableMovement();
+        
+        // Trigger attack animation
+        playerAnimator.SetBool("Attack", true);
+        
+        // Fire the arrow midway through the animation
+        yield return new WaitForSeconds(attackDuration * 0.5f);
+        
         _shotsRemaining--;
         Debug.Log($"[Bow] Fired! {_shotsRemaining}/{magazineSize} arrows left.");
-
         Shoot();
-
+        
+        // Wait for the rest of the animation to complete
+        yield return new WaitForSeconds(attackDuration * 0.5f);
+        
+        // End attack animation
+        playerAnimator.SetBool("Attack", false);
+        
+        // Re-enable player movement
+        playerMovement.EnableMovement();
+        
+        _isAttacking = false;
+        
         if (_shotsRemaining <= 0)
             StartCoroutine(ReloadRoutine());
     }
-
+    
     private void Shoot()
     {
         GameObject proj = Instantiate(projectilePrefab, transform.position, transform.rotation);
         Rigidbody2D rb = proj.GetComponent<Rigidbody2D>();
-
-        //we can change the hard corded projectile speed to the shop bought one
-        //also for lifetime they stay on the screen longer which can be adjusted by a stat
         rb.velocity = proj.transform.right * projectileSpeed;
         Destroy(proj, lifeTime);
     }
-      private IEnumerator ReloadRoutine()
+    
+    private IEnumerator ReloadRoutine()
     {
         _isReloading = true;
+        
+        // Optional: You could trigger a reload animation here
+        // playerAnimator.SetBool("Reloading", true);
+        
         yield return new WaitForSeconds(reloadDuration);
+        
+        // Optional: End reload animation
+        // playerAnimator.SetBool("Reloading", false);
+        
         _shotsRemaining = magazineSize;
-        _isReloading  = false;
+        _isReloading = false;
     }
 }
