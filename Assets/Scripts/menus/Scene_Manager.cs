4d2079a3f78fb60402d5a94764dcdf8e2ec2baeb
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2025-05-19 22:42:19 -0500

    Start room cleanup and forest edge fix?

--- Assets/Scripts/menus/Scene_Manager.cs
+++ Assets/Scripts/menus/Scene_Manager.cs
@@ -7,14 +7,16 @@ public class Scene_Manager : MonoBehaviour
 {
     public float before_load = 10f;
     public string name;
-    
-    
-    public void LoadScene (){
-        Debug.Log("help");
+
+
+    public void LoadScene()
+    {
+        // Debug.Log("help");
         StartCoroutine(Load_after_delay());
     }
 
-    private IEnumerator Load_after_delay(){
+    private IEnumerator Load_after_delay()
+    {
         yield return new WaitForSeconds(before_load);
         SceneManager.LoadScene(name);
 
